87139047bd3ae5c345e5e7a732ef423b4957ffe6
Chris Holland <sareuth@gmail.com>
2026-08-13 00:05:31 +0100

    Add task toggle state handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,46 @@
+import { useState } from "react";
 import { TaskList } from "./TaskList";
 import type { Task } from "./types";
 
-const tasks: Task[] = [
-  {
-    id: "1",
-    title: "Create Workbench repository",
-    completed: true,
-  },
-  {
-    id: "2",
-    title: "Render first React components",
-    completed: false,
-  },
-  {
-    id: "3",
-    title: "Add testing",
-    completed: false,
-  },
-];
-
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      id: "1",
+      title: "Create Workbench repository",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "Render first React components",
+      completed: false,
+    },
+    {
+      id: "3",
+      title: "Add testing",
+      completed: false,
+    },
+  ]);
+
+  const handleToggle = (id: string) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return {
+          id: task.id,
+          title: task.title,
+          completed: !task.completed
+        }
+      } else {
+        return task;
+      }
+    });
+
+    setTasks (newTasks);
+  };
+
   return (
     <main>
       <h1>Workbench</h1>
-      <TaskList tasks={tasks} />
+      <TaskList tasks={tasks} handleToggle={handleToggle}/>
     </main>
   );
 }
--- src/TaskItem.tsx
+++ src/TaskItem.tsx
@@ -2,12 +2,13 @@ import type { Task } from "./types";
 
 interface TaskItemProps {
   task: Task;
+  handleToggle: (id: string) => void
 }
 
-export function TaskItem({ task }: TaskItemProps) {
+export function TaskItem({ task, handleToggle }: TaskItemProps) {
   return (
     <li>
-      <input type="checkbox" checked={task.completed} readOnly />
+      <input type="checkbox" checked={task.completed} onChange={() => handleToggle(task.id)} />
       <span>{task.title}</span>
     </li>
   );
